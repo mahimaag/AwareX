1a51d80574530ad57b64d4916d04553022955b0c
Mahima Agrawal <mahi9.goyal@gmail.com>
2017-06-13 11:04:33 +0530

    Added slick slider for sliding the usage category

--- src/components/UsageCard/index.js
+++ src/components/UsageCard/index.js
@@ -1,16 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CircularProgressbar from '../ProgressBar';
+import UsageCategorySlider from '../UsageCategorySlider';
+import SmsUsageCard from '../SmsUsageCard';
+import DataUsageCard from '../DataUsageCard';
+import VoiceUsageCard from '../VoiceUsageCard';
+import config from '../config.json';
 
 class UsageCard extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    getSliderComponents(sliderItems){
+        console.log('hjhbhjbh', sliderItems)
+        let items;
+        items = sliderItems.map(item => {
+            if (item) {
+                switch (item) {
+                    case 'Data':
+                        return(
+                            <div>
+                                <DataUsageCard/>
+                            </div>
+                        )
+                    case 'Sms':
+                        return(
+                            <div>
+                                <SmsUsageCard/>
+                            </div>
+                        )
+                    case 'Voice':
+                        return(
+                            <div>
+                                <VoiceUsageCard/>
+                            </div>
+                        );
+                    default:
+                        return null;
+                }
+            }
+        });
+        return items;
+
+    }
 
     render() {
+        const sliderItems = config.usageCards;
+        const sliderComponents = this.getSliderComponents(sliderItems);
+
         return (
-            <CircularProgressbar percentage={10} />
+            <div>
+                <UsageCategorySlider slides={sliderComponents}/>
+                <CircularProgressbar percentage={10} />
+            </div>
         );
     }
 }
